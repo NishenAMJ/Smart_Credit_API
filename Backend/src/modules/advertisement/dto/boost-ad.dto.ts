ac7c2ca23b9dec3fc21e4182fe6402d6fe3c6a56
kaushani rasadika <rasadikak@gmail.com>
2026-04-26 08:06:03 +0530

    created boost-ad.dto

---
+++ Backend/src/modules/advertisement/dto/boost-ad.dto.ts
@@ -0,0 +1,25 @@
+import {
+  IsNumber,
+  IsString,
+  IsNotEmpty,
+  IsPositive,
+  IsEnum,
+} from 'class-validator';
+
+// Boost packages available
+export type BoostPackage = '7days' | '14days' | '30days';
+
+export class BoostAdDto {
+  @IsEnum(['7days', '14days', '30days'], {
+    message: 'Package must be 7days, 14days or 30days',
+  })
+  package?: BoostPackage;
+
+  @IsNumber()
+  @IsPositive({ message: 'Amount must be positive' })
+  amount?: number;
+
+  @IsString()
+  @IsNotEmpty({ message: 'Payment reference is required' })
+  paymentReference?: string;
+}
