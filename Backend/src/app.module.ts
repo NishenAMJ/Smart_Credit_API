805bc7683bbe98fef4e72ff290b43252815707f6
mahinsaK <kadmahinsa@gmail.com>
2026-04-28 21:51:22 +0530

    again

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AnalyticsModule } from './analytics/analytics.module';
-import { FirebaseModule } from './firebase/firebase.module';
-import { DashboardModule } from './dashboard/dashboard.module';
-import { LenderAdsModule } from './lender-ads/lender-ads.module';
-import { LenderNotificationsModule } from './lender-notifications/lender-notifications.module';
-import { LenderProfileModule } from './lender-profile/lender-profile.module';
-import { LenderSettingsModule } from './lender-settings/lender-settings.module';
-import { LoanRequestsModule } from './loan-requests/loan-requests.module';
-import { RecentTransactionsModule } from './recent-transactions/recent-transactions.module';
+import { AnalyticsModule } from './modules/lender/analytics/analytics.module';
+import { FirebaseModule } from './modules/lender/firebase/firebase.module';
+import { DashboardModule } from './modules/lender/dashboard/dashboard.module';
+import { LenderAdsModule } from './modules/lender/lender-ads/lender-ads.module';
+import { LenderNotificationsModule } from './modules/lender/lender-notifications/lender-notifications.module';
+import { LenderProfileModule } from './modules/lender/lender-profile/lender-profile.module';
+import { LenderSettingsModule } from './modules/lender/lender-settings/lender-settings.module';
+import { LoanRequestsModule } from './modules/lender/loan-requests/loan-requests.module';
+import { RecentTransactionsModule } from './modules/lender/recent-transactions/recent-transactions.module';
 
 @Module({
   imports: [
